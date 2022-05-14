1dadc3e02b46715a669274143fa95111cd42b2ca
keegi <keegi@kakao.com>
2022-05-14 21:29:32 +0900

    first commit

---
+++ js/quotes.js
@@ -0,0 +1,55 @@
+const quotes = [
+    {
+        quote : "삶이 있는 한 희망은 있다",
+        author : "키케로",
+    },
+    {
+        quote : "하루에 3시간을 걸으면 7년 후에 지구를 한바퀴 돌 수 있다",
+        author : "사무엘존슨",
+    },
+    {
+        quote : "언제나 현재에 집중할수 있다면 행복할것이다",
+        author : "파울로 코엘료",
+    },
+    {
+        quote : "진정으로 웃으려면 고통을 참아야하며 , 나아가 고통을 즐길 줄 알아야 해",
+        author : "찰리 채플린",
+    },
+    {
+        quote : "직업에서 행복을 찾아라. 아니면 행복이 무엇인지 절대 모를 것이다",
+        author : "엘버트 허버드",
+    },
+    {
+        quote : "신은 용기있는자를 결코 버리지 않는다",
+        author : "켄러",
+    },
+    {
+        quote : "네 믿음은 네 생각이 된다 . 네 생각은  네 말이 된다. 네말은 네 행동이 된다 네행동은 네 습관이된다 . 네 습관은 네 가치가 된다 . 네 가치는 네 운명이 된다",
+        author : "간디",
+    },
+    {
+        quote : "어리석은 자는 멀리서 행복을 찾고, 현명한 자는 자신의 발치에서 행복을 키워간다",
+        author : "제임스 오펜하임",
+    },
+    {
+        quote : "한번의 실패와 영원한 실패를 혼동하지 마라",
+        author : "F.스콧 핏제랄드",
+    },
+    {
+        quote : "자신감 있는 표정을 지으면 자신감이 생긴다",
+        author : "찰스다윈",
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
+
+
+
+
+
---
+++ js/weather.js
@@ -0,0 +1,25 @@
+const API_KEY = "f4893174290c96768a68bcf4a1825453"
+
+function onGeoOk(position)
+{
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+    .then((Response) => Response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError()
+{
+    alert('날씨 정보를 찾을수 없습니다.');
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+
